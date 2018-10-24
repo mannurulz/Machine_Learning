4f8354ea9bb250397541f019c2a06d6d40a9412e
mannurulz <mannurulz@gmail.com>
2018-10-24 17:59:56 +0800

    Predict * Confusion Matrix

--- Email_Classification_MachineLearning.py
+++ Email_Classification_MachineLearning.py
@@ -23,6 +23,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 import itertools
 from sklearn.metrics import confusion_matrix
+from pandas.tests.groupby.test_function import test_size
 
 data = pd.read_excel("C:/Users/mmishra/Desktop/SampleData.xlsx")
 df = pd.DataFrame(data, columns=['Policy','matches','Status','Subject','has attachment','VIP','department code'])
@@ -66,3 +67,66 @@ df = handle_non_numerical_data(df)
 Y = df[['Status']].copy()
 #Dropping Target variable from feature dataset
 X = np.array(df.drop(['Status'],1).astype(float))
+
+print(X.shape, Y.shape)
+
+# Split data for training & testing 
+x_tr, x_te, y_tr, y_te = train_test_split(X,Y, test_size=0.2)
+
+RFC = RandomForestClassifier()
+RFC.fit(x_tr,y_tr)
+
+print(RFC.feature_importances_)
+Yp = RFC.predict(x_te)
+RFC.score(x_te, y_te)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
