9d98bff4632ba51711f024edf524ba714880bf87
mannurulz <mannurulz@gmail.com>
2018-10-24 15:46:11 +0800

    Handle non numerical data

--- Email_Classification_MachineLearning.py
+++ Email_Classification_MachineLearning.py
@@ -30,8 +30,39 @@ df.fillna(0, inplace=True)
 
 columns = df.columns.values
 
-#Source data analysis
+#Source data analysis & Plotting
 
 fig = plt.figure(figsize=(8,6))
 df.groupby('Status').Status.count().plot.bar(ylim=0)
 plt.show()
+
+
+# Preprocessing
+def handle_non_numerical_data(df):
+    columns = df.columns.values
+
+    for column in columns:
+        text_digit_vals = {}
+        def convert_to_int(val):
+            return text_digit_vals[val]
+
+        if df[column].dtype != np.int64 and df[column].dtype != np.float64:
+            column_contents = df[column].values.tolist()
+            unique_elements = set(column_contents)
+            x = 0
+            for unique in unique_elements:
+                if unique not in text_digit_vals:
+                    text_digit_vals[unique] = x
+                    x+=1
+
+            df[column] = list(map(convert_to_int, df[column]))
+
+    return df
+
+df = handle_non_numerical_data(df)
+
+
+# Create Target
+Y = df[['Status']].copy()
+#Dropping Target variable from feature dataset
+X = np.array(df.drop(['Status'],1).astype(float))
