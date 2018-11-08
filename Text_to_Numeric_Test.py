635e4ac2feb1e92508af50d974569f96bdb5dfb8
mannurulz <mannurulz@gmail.com>
2018-11-08 15:28:08 +0800

    v2

--- Text_to_Numeric_Test.py
+++ Text_to_Numeric_Test.py
@@ -7,12 +7,34 @@ vectorizer = CountVectorizer()
 # tokenize and build vocab
 vectorizer.fit(text)
 # summarize
-print(vectorizer.vocabulary_)
+#print(vectorizer.vocabulary_)
 # encode document
 vector = vectorizer.transform(text)
 # summarize encoded vector
-print(vector.shape)
-print(type(vector))
-print(vector.toarray())
+#print(vector.shape)
+#print(type(vector))
+#print(vector.toarray())
 
+# Test Technique 2 #
+import pandas as pd
+#import xlrd
+from sklearn.feature_extraction.text import TfidfVectorizer
+# list of text documents
+#text = pd.read_excel("C:/Users/mmishra/Downloads/Code/R_n_D/Data/SampleData.xlsx")
+#data = pd.DataFrame(text)
+#print(data)
+text =  ["The quick brown fox jumped over the lazy dog.",
+		"The dog.",
+		"The fox"]
+# create the transform
+vectorizer = TfidfVectorizer()
+# tokenize and build vocab
+vectorizer.fit(text)
+# summarize
 print(vectorizer.vocabulary_)
+print(vectorizer.idf_)
+# encode document
+vector = vectorizer.transform([text[0]])
+# summarize encoded vector
+print(vector.shape)
+print(vector.toarray())
